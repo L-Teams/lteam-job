b08346323024f31dc1cff66098af79b4ce4ddd74
guicheng.huang <guicheng.huang@bestwehotel.com>
2017-04-17 10:31:19 +0800

    增加接口定义。

---
+++ lteam-job-core/src/main/java/com/lteam/job/core/routing/IRouteStrategy.java
@@ -0,0 +1,10 @@
+package com.lteam.job.core.routing;
+/**
+ * @Description:路由策略接口
+ * @author guicheng.huang
+ * @date: 2017年4月17日 上午10:28:31
+ * @version V0.0.1
+ */
+public interface IRouteStrategy {
+
+}
---
+++ lteam-job-core/src/main/java/com/lteam/job/core/scheduler/IScheduler.java
@@ -0,0 +1,10 @@
+package com.lteam.job.core.scheduler;
+/**
+ * @Description:调度器接口
+ * @author guicheng.huang
+ * @date: 2017年4月17日 上午10:29:25
+ * @version V0.0.1
+ */
+public interface IScheduler {
+
+}
