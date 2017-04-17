280c86f9130b03e484757437317ad486ee113ef7
guicheng.huang <guicheng.huang@bestwehotel.com>
2017-04-17 18:49:53 +0800

    增加接口定义

---
+++ lteam-job-commom/src/main/java/com/lteam/job/common/zookeeper/IZookeeperUtil.java
@@ -0,0 +1,10 @@
+package com.lteam.job.common.zookeeper;
+/**
+ * @Description:Zookeeper工具接口定义
+ * @author guicheng.huang
+ * @date: 2017年4月17日 上午10:21:11
+ * @version V0.0.1
+ */
+public interface IZookeeperUtil {
+
+}
---
+++ lteam-job-commom/src/main/java/com/lteam/job/common/zookeeper/ZkClientUtil.java
@@ -0,0 +1,10 @@
+package com.lteam.job.common.zookeeper;
+/**
+ * @Description:基于ZkClient实现zookeeper操作
+ * @author guicheng.huang
+ * @date: 2017年4月17日 下午6:46:18
+ * @version V0.0.1
+ */
+public abstract class ZkClientUtil implements IZookeeperUtil {
+
+}
