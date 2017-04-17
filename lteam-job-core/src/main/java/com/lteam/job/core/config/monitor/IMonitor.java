3aeb9a1c23c4de9b9cbe5e632272b63c1b1754eb
huangguicheng <862896568@qq.com>
2017-04-17 10:35:25 +0800

    增加监控告警接口

---
+++ lteam-job-core/src/main/java/com/lteam/job/core/config/monitor/IMonitor.java
@@ -0,0 +1,10 @@
+package com.lteam.job.core.config.monitor;
+/**
+ * @Description:监控告警接口
+ * @author guicheng.huang
+ * @date: 2017年4月17日 上午10:34:26
+ * @version V0.0.1
+ */
+public interface IMonitor {
+
+}
